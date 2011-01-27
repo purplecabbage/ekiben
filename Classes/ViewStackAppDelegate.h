492c23927847c7bb74f649fc874cb50042ea50b5
Jesse <jesse.macfadyen@nitobi.com>
2011-01-27 15:26:38 -0800

    Here it is...
    Signed-off-by: Jesse <jesse.macfadyen@nitobi.com>

---
+++ Classes/ViewStackAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ViewStackAppDelegate.h
+//  ViewStack
+//
+//  Created by Jesse MacFadyen on 10-11-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface ViewStackAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ViewStack
+//
+//  Created by Jesse MacFadyen on 10-11-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"ViewStackAppDelegate");
+    [pool release];
+    return retVal;
+}
